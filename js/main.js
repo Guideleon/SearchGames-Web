7394ba3469475c68e4799cab6ef93cf8aeeb2da0
joaog <Guideleon>
2020-04-15 02:21:34 -0300

    Changed to white interface

--- js/main.js
+++ js/main.js
@@ -1,93 +1,93 @@
 var nextPage = '';
 
 $(document).ready(function () {
-  let searchParams = new URLSearchParams(window.location.search);
+    let searchParams = new URLSearchParams(window.location.search);
 
-  if (searchParams.has('search')) {
-    searchGames(searchParams.get('search'));
-  }
+    if (searchParams.has('search')) {
+        searchGames(searchParams.get('search'));
+    }
 
-  $('#searchForm').on('submit', function (e) {
-    let searchText = $('#searchText').val();
-    var params = {
-      search: searchText
-    };
-    let searchParams = new URLSearchParams(window.location.search)
-    console.log(searchParams);
-    var str = jQuery.param(params);
-    searchGames(str)
-  });
+    $('#searchForm').on('submit', function (e) {
+        let searchText = $('#searchText').val();
+        var params = {
+            search: searchText
+        };
+        let searchParams = new URLSearchParams(window.location.search)
+        console.log(searchParams);
+        var str = jQuery.param(params);
+        searchGames(str)
+    });
 });
 
 document.addEventListener("DOMContentLoaded", () => {
-  let options = {
-    root: null,
-    rootMargins: "0px",
-    threshold: 0.5
-  };
-  const observer = new IntersectionObserver(handleIntersect, options);
-  observer.observe(document.querySelector("footer"));
+    let options = {
+        root: null,
+        rootMargins: "0px",
+        threshold: 0.5
+    };
+    const observer = new IntersectionObserver(handleIntersect, options);
+    observer.observe(document.querySelector("footer"));
 
 
-  let searchParams = new URLSearchParams(window.location.search);
+    let searchParams = new URLSearchParams(window.location.search);
 
-  if (!searchParams.has('search')) {
-    getURLGames('https://api.rawg.io/api/games?dates=2019-10-10,2020-10-10&ordering=-added');
-  }
+    if (!searchParams.has('search')) {
+        getURLGames('https://api.rawg.io/api/games?dates=2019-10-10,2020-10-10&ordering=-added');
+    }
 });
 
 function handleIntersect(entries) {
-  if (entries[0].isIntersecting) {
-    getURLGames(nextPage);
-  }
+    if (entries[0].isIntersecting) {
+        getURLGames(nextPage);
+    }
 }
 
 function getURLGames(URL) {
-  axios.get(URL).then(function (response) {
-    nextPage = response.data.next;
+    axios.get(URL).then(function (response) {
+        nextPage = response.data.next;
 
-    let output = createCard(response);
+        let output = createCard(response);
 
-    $('#games').append(output);
-  }).catch(function (err) {
-    console.log(err);
-  });
+        $('#games').append(output);
+    }).catch(function (err) {
+        console.log(err);
+    });
 
 }
 
 function searchGames(searchText) {
-  axios.get('https://api.rawg.io/api/games?search=' + searchText).then(function (response) {
-    nextPage = response.data.next;
+    axios.get('https://api.rawg.io/api/games?search=' + searchText).then(function (response) {
+        nextPage = response.data.next;
 
-    let output = createCard(response);
+        let output = createCard(response);
 
-    $('#games').html(output);
-  }).catch(function (err) {
-    console.log(err);
-  });
+        $('#games').html(output);
+    }).catch(function (err) {
+        console.log(err);
+    });
 }
 
 function createCard(response) {
-  let games = response.data.results;
-  let output = '';
-  $.each(games, (index, game) => {
-    output += ` 
+    let games = response.data.results;
+    let output = '';
+    $.each(games, (index, game) => {
+        output += ` 
             <div class="col-md-3">
                 <div class="card w3-animate-opacity">
                     <img class="card-img-top" src="${game.background_image}">
 					<div class="card-body">
 						<h5 class="card-title">${game.name}</h5>
-						<p class="card-text">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec ornare, risus eu dignissim suscipit, tellus odio sollicitudin purus, quis mollis sapien magna et eros.</p>
-						<a class="btn btn-primary float-bottom btn-md" onclick="gameSelected('${game.slug}')">Game Details</a>
+						<p class="card-text">Lorem ipsum dolor sit amet</p>
+						<a class="btn btn-primary text-light btn-md" onclick="gameSelected('${game.slug}')">Game Details</a>
 					</div> 
                 </div>
             </div>
             `;
-  });
-  return output;
+    });
+    return output;
 }
 
 function gameSelected(slug) {
-  window.location = 'game' + '?id=' + slug;
-  return false;
+    window.location = 'game' + '?id=' + slug;
+    return false;
 }
